98419d7083a8037dd8d4b82518ca64959bbd147d
hikam074 <hikam074@gmail.com>
2026-01-25 15:52:21 +0700

    fix: admin should coming soon

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,23 +1,8 @@
 'use client'
-
-import { useEffect,useState } from "react"
-import { supabase } from "@/lib/supabase"
-import { Anime } from "@/types/anime"
-
 export default function AdminPage() {
-    const [animes, setAnimes] = useState<Anime[]>([])
-    useEffect(() => {
-        supabase.from('animes').select('*').then(({ data }) => {
-            if (data) setAnimes(data as Anime[])
-        })
-    }, [])
-
     return (
         <div>
             <h1>Admin</h1>
-            {animes.map( a => (
-                <div key={a.id} className="text-center border-1 m-4">{a.title}</div>
-            ))}
         </div>
     )
 }
