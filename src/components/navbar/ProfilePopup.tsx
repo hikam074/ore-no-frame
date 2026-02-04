d37844414cef4d0085ba7bc0b5eb14c1d9122e38
hikam074 <hikam074@gmail.com>
2026-02-05 00:52:06 +0700

    fix: typo email

--- src/components/navbar/ProfilePopup.tsx
+++ src/components/navbar/ProfilePopup.tsx
@@ -49,7 +49,7 @@ const ProfilePopup = ({ open, onClose }: ProfilePopupProps) => {
         "
         >
             <p className="text-accent text-sm font-semibold">{user ? user?.name : ''}</p>
-            <p className="text-text_muted text-xs font-thin">{capitalize(user ? user?.email : '')}</p>
+            <p className="text-text_muted text-xs font-thin">{user ? user?.email : ''}</p>
             <p className="text-text_muted text-xs font-thin">{capitalize(user ? user?.role : 'Silahkan login untuk mengakses fitur lengkap')}</p>
             {user &&
                 <button className="text-accent border px-2 py-1 text-xs mt-4" onClick={logout}>Logout</button>
