067ec3af244f5486a094072c0d439831a0ccaafc
hikam074 <hikam074@gmail.com>
2026-02-18 17:33:22 +0700

    bug: logout then login again dint change the data

--- src/app/(public)/(bare)/auth/login/page.tsx
+++ src/app/(public)/(bare)/auth/login/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
             else {
                 setFlash("success", "Login success! Welcome");
 
-                router.refresh();
+                await router.refresh();
                 router.push("/");
             }
         } catch (err: unknown) {
