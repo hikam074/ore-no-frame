96effff3eb577d048884f72da05020dfa6ec6648
hikam074 <hikam074@gmail.com>
2026-03-08 17:13:27 +0700

    fix : add additional endpoint

--- src/lib/consumers/home.consumer.ts
+++ src/lib/consumers/home.consumer.ts
@@ -2,5 +2,5 @@ import { Anime } from "@/types/anime"
 import { apiFetch } from "../api/fetcher"
 
 export function fetchHomeAnime() {
-  return apiFetch<Anime[]>(`/anime/suggestion`)
+  return apiFetch<Anime[]>(`/anime/suggestion/`)
 }
